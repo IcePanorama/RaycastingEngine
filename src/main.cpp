9baa0cf141a5f395d7b4591352804e91763cf13f
IcePanorama <thriftedroot@protonmail.com>
2024-07-19 15:27:43 -0400

    Drawing 2D map.

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ uint8_t test_map[64] = {
 };
 /* clang-format on */
 
-// void draw_2d_map (Map &m);
+void draw_2d_map (Map &m, uint32_t start_x, uint32_t start_y);
 
 int
 main (void)
@@ -30,10 +30,48 @@ main (void)
   while (!WindowShouldClose ())
     {
       BeginDrawing ();
+      ClearBackground (LIGHTGRAY);
       Map map (8, 8, test_map);
-      ClearBackground (RAYWHITE);
+      draw_2d_map (map, 0, 0);
       EndDrawing ();
     }
 
   return 0;
+}
+
+/**
+ *  draw_2d_map
+ *
+ *  Draws a 2D representation of the given map, placing its top right corner at
+ *  the given position, `(start_x, start_y)`. Note however, that this function
+ *  uses a tiny `GAP_OFFSET` to better visibility, therefore the actual top
+ *  right corner of the map on screen will be
+ *  `(start_x + GAP_OFFSET / 2, start_y + GAP_OFFSET / 2)`.
+ *
+ *  @param  m       the map you wish to draw
+ *  @param  start_x the x position of the top right corner of the map on screen
+ *  @param  start_y the y position of the top right corner of the map on screen
+ */
+void
+draw_2d_map (Map &m, uint32_t start_x, uint32_t start_y)
+{
+  const uint8_t GAP_OFFSET = 3;
+  const Color FLOOR_COLOR = BLACK;
+  const Color WALL_COLOR = YELLOW;
+
+  uint8_t width = m.get_width ();
+  uint8_t length = m.get_length ();
+  const uint8_t SCALE = WINDOW_HEIGHT / length;
+  uint8_t size = width * length;
+
+  for (uint8_t i = 0; i < size; i++)
+    {
+      uint8_t row = i / width;
+      uint8_t col = i % width;
+
+      Color c = m.get_map ()[i] == 1 ? WALL_COLOR : FLOOR_COLOR;
+      DrawRectangle (start_x + (col * SCALE + GAP_OFFSET),
+                     start_y + (row * SCALE + GAP_OFFSET),
+                     SCALE - 2 * GAP_OFFSET, SCALE - 2 * GAP_OFFSET, c);
+    }
 }
