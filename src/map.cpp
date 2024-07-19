13c7eec24bcde5f3c3ddcac5e730eac50023f8d0
IcePanorama <thriftedroot@protonmail.com>
2024-07-19 15:33:52 -0400

    Added javadoc style comments to Map.hpp/.cpp

--- src/map.cpp
+++ src/map.cpp
@@ -1,5 +1,14 @@
 #include "map.hpp"
 
+/**
+ *  Map::Map
+ *
+ *  Constructor for the `Map` class.
+ *
+ *  @param  width   the width of the map
+ *  @param  length  the length of the map
+ *  @param  map     the actual map data itself
+ */
 Map::Map (uint8_t width, uint8_t length, uint8_t *map)
 {
   this->width = width;
@@ -7,18 +16,39 @@ Map::Map (uint8_t width, uint8_t length, uint8_t *map)
   this->map = map;
 }
 
+/**
+ *  Map::get_width
+ *
+ *  Returns the width of this map.
+ *
+ *  @return uint8_t of the map width
+ */
 uint8_t
 Map::get_width (void) const
 {
   return this->width;
 }
 
+/**
+ *  Map::get_length
+ *
+ *  Returns the length of this map.
+ *
+ *  @return uint8_t of the map length
+ */
 uint8_t
 Map::get_length (void) const
 {
   return this->length;
 }
 
+/**
+ *  Map::get_map
+ *
+ *  Returns the underlying array of this map.
+ *
+ *  @return the underlying map array
+ */
 uint8_t *
 Map::get_map (void) const
 {
--- src/map.hpp
+++ src/map.hpp
@@ -3,6 +3,13 @@
 
 #include <cstdint>
 
+/**
+ *  Map
+ *
+ *  Wrapper class that stores the `width` and `length` of a given map as well
+ *  as the map itself. Note that for simplicities sake, the map is stored in a
+ *  1D array.
+ */
 class Map
 {
   uint8_t width;
